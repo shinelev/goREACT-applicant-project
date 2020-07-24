f9b70f496267bfa7ab0084813009382405ef0554
Dmitry Shinelev <d.s.shinelev@gmail.com>
2020-07-24 06:42:16 +0300

    Removed unnecessary migration file, added new ones for files, added auth

---
+++ database/migrations/2020_07_23_212151_create_files_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->string('file_name');
+            $table->string('file_type');
+            $table->string('file_name_saved');
+            $table->string('title')->nullable();
+            $table->string('description')->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('picture_storage');
+    }
+}
